d81c8d90d12d88ef91e049eb3d1161bd994f6a4b
mazay <andymazay84@gmail.com>
2023-08-28 10:40:19 +0300

    Lesson1 cleaned

---
+++ Lesson1/Main.java
@@ -0,0 +1,14 @@
+import FirstTask.PrintThreeWords;
+import FourthTask.CompareNumbers;
+import SecondTask.CheckSumSign;
+import ThirdTask.PrintColor;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+
+    }
+}
